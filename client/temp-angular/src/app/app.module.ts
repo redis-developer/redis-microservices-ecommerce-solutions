b395cd3bbb47e1f80425376f1b931bd6544e198f
PrasanKumar <prasankumar93@gmail.com>
2023-02-24 17:19:28 +0530

    order history, confirmation page

--- client/temp-angular/src/app/app.module.ts
+++ client/temp-angular/src/app/app.module.ts
@@ -1,21 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 import { FormsModule } from '@angular/forms';
 import { HoverCssDirective } from './directives/hove-css.directive';
+import { OrderComponent } from './components/orders/orders.component';
+import { Orderconfirmation } from './components/order-confirmation/order-confirmation.component';
+
+const appRouter: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'orders', component: OrderComponent },
+  { path: 'orderconfirm/:id', component: Orderconfirmation }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HoverCssDirective,
+    HomeComponent,
+    OrderComponent,
+    Orderconfirmation
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRouter)
   ],
   providers: [],
   bootstrap: [AppComponent]
